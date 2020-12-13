d0c55b99dece2f679a00ef5a69ec80b7de80f25b
john <vovanchau27@gmail.com>
2020-12-13 21:19:10 +0700

    add packet

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.js', '.ios.js', '.android.js'],
+        alias: {
+          utils: './src/Utils',
+          reduxs: './src/Reduxs',
+          assets: './src/Assets',
+          screens: './src/Screens',
+          components: './src/Components',
+          navigations: './src/Navigations',
+        },
+      },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
 };
